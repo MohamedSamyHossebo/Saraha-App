e8177f5d2c40250db370310708df5886c0ff2346
Mohamed Samy <116973612+MohamedSamyHossebo@users.noreply.github.com>
2026-02-20 17:42:09 +0200

    init

--- src/app.controller.js
+++ src/app.controller.js
@@ -1,15 +1,25 @@
+import { authRouter, userRouter, messageRouter } from "./Modules/index.js"
+import { badRequest, globalErrorHandler } from "./Utils/response/error.response.js";
 
 const bootstrap = async (app, express) => {
     app.use(express.json())
+
     app.get("/", (req, res) => {
-        res.send("Hello World!");
-    });
-    app.all("/*dummy", (req, res) => {
-        res.status(404).json({
-            status: "fail",
-            message: "Route not found",
+        res.status(200).json({
+            status: "success",
+            message: "Sara7a backend is running",
         });
     });
-    
+
+
+    app.use("/auth", authRouter)
+    app.use("/user", userRouter)
+    app.use("/message", messageRouter)
+
+    app.all("/*dummy", (req, res) => {
+       throw badRequest ({ message: "Route not found" })
+
+    });
+    app.use(globalErrorHandler);
 }
 export default bootstrap
