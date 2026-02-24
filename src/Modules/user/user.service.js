d2c9d279bbd2c1aff2c7b1e76ecea42243a1576b
Mohamed Samy <116973612+MohamedSamyHossebo@users.noreply.github.com>
2026-02-24 23:39:36 +0200

    feat: add user profile fetching with phone number decryption

--- src/Modules/user/user.service.js
+++ src/Modules/user/user.service.js
@@ -1,7 +1,11 @@
 import successResponse from "../../Utils/response/success.response.js";
+import { decrypt } from "../../Utils/security/encryption.security.js";
 
 
 export const profile = async (req, res) => {
+    if (req.user) {
+        req.user.phoneNumber = await decrypt(req.user.phoneNumber)
+    }
     return successResponse({
         res, statusCode: 200,
         message: "User profile fetched successfully",
