81ab472618f9f952aed8c5d517fe651fd17f628c
Mohamed Samy <116973612+MohamedSamyHossebo@users.noreply.github.com>
2026-02-24 23:32:53 +0200

    feat: add authentication middleware for token decoding and user verification.

--- src/Middlewares/auth.middleware.js
+++ src/Middlewares/auth.middleware.js
@@ -1,29 +1,39 @@
 
-import { TOKEN_TYPE_ENUM } from "../Utils/enums/user.enum.js";
+import { TOKEN_TYPE_ENUM, SIGNATURE_ENUM } from "../Utils/enums/user.enum.js";
 import { badRequest, notFound } from "../Utils/response/error.response.js";
 import { getSignature, verifyToken } from "../Utils/tokens/token.js";
 import * as dbService from "../DB/database.repository.js";
 import userModel from "../DB/Models/User/user.model.js";
 
 export const decodedToken = async ({ authorization, tokenType = TOKEN_TYPE_ENUM.ACCESS }) => {
-    const { Bearer, token } = authorization.split(" ") || [];
-    if (!Bearer || !token) { throw badRequest({ message: "Authorization header is required" }); }
-    console.log(Bearer, token);
-    let signature = await getSignature({ signatureLevel: Bearer })
-    const decoded = await verifyToken({ token, secret: tokenType === TOKEN_TYPE_ENUM.ACCESS ? signature.accessSignature : signature.refreshSignature, tokenType })
+    if (!authorization) { throw badRequest({ message: "Authorization header is required" }); }
+
+    const [prefix, token] = authorization.split(" ");
+    if (!prefix || !token) { throw badRequest({ message: "Invalid Authorization header" }); }
+    console.log(prefix, token);
+    const signatureLevel = prefix === "Admin" ? SIGNATURE_ENUM.ADMIN : SIGNATURE_ENUM.USER;
+    const signature = getSignature({ signatureLevel })
+    console.log(signature);
+    const decoded = verifyToken({
+        token,
+        secret: tokenType === TOKEN_TYPE_ENUM.ACCESS ? signature.accessSignature : signature.refreshSignature
+    })
+    console.log(decoded);
     const user = await dbService.findById({ model: userModel, id: decoded.id })
     console.log(user);
     if (!user) { throw notFound({ message: "User not found" }); }
     return { user, decoded }
-
 }
 
 export const authentication = ({ tokenType = TOKEN_TYPE_ENUM.ACCESS }) => {
     return async (req, res, next) => {
-        const { user, decoded } = (await decodedToken({ authorization: req.headers.authorization, tokenType })) || {}
-        req.user = user
-        req.decoded = decoded
-        console.log(req.user);
-        return next()
+        try {
+            const { user, decoded } = await decodedToken({ authorization: req.headers.authorization, tokenType })
+            req.user = user
+            req.decoded = decoded
+            return next()
+        } catch (error) {
+            return next(error)
+        }
     }
 }
