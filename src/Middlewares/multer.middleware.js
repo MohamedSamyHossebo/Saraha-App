e553b976660b9b8e50b17fc407200e0ad3176ce5
Mohamed Samy <116973612+MohamedSamyHossebo@users.noreply.github.com>
2026-03-07 23:22:53 +0200

    Upload Folder Path Resolution

--- src/Middlewares/multer.middleware.js
+++ src/Middlewares/multer.middleware.js
@@ -1,15 +1,29 @@
 import multer from "multer";
 import path from "node:path";
+import fs from "node:fs";
+
+const allowedMimeTypes = ["image/jpeg", "image/png", "image/jpg"];
 
 export const localFileUpload = () => {
     const storage = multer.diskStorage({
         destination: (req, file, cb) => {
-            cb(null, path.resolve("./src/uploads"))
+            if (!fs.existsSync(path.resolve("uploads"))) {
+                fs.mkdirSync(path.resolve("uploads"), { recursive: true })
+            }
+            cb(null, path.resolve("uploads"))
         },
         filename: (req, file, cb) => {
             const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
             cb(null, uniqueSuffix + path.extname(file.originalname))
         }
     })
-    return multer({ storage })
+    return multer({
+        storage, fileFilter: (req, file, cb) => {
+            if (allowedMimeTypes.includes(file.mimetype)) {
+                cb(null, true)
+            } else {
+                cb(null, false)
+            }
+        }
+    })
 }
